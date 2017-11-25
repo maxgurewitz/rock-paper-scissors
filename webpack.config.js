41af722780e00af87783332c09da028fe64d90dc
maxgurewitz <maxthegeek1@gmail.com>
2017-11-25 14:02:27 -0500

    webpack watch task

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 module.exports = {
   entry: './src/js/index.js',
+  devServer: {
+    contentBase: './dist'
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
