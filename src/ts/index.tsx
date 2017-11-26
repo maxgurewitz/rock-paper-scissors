d7c20fc9b1af1621cc47a5bb2b5cef68e9aa6bac
maxgurewitz <maxthegeek1@gmail.com>
2017-11-25 22:25:09 -0500

    wire up redux actions

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -1,35 +1,86 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 import { connect, Provider } from 'react-redux'
+import {clone} from 'lodash';
 import { createStore } from 'redux'
 
-interface NoOp {};
-
-type Msg = NoOp;
-
-const update = (state : State, action : Msg) => { 
-    return state;
+interface NoOp {
+    type: 'noOp'
 };
 
-interface State {
-    selections: number;
+interface SelectHand {
+    type: 'selectHand',
+    hand: Hand
+}
+
+interface Init {
+    type: '@@redux/INIT'
+}
+
+type Msg = NoOp | SelectHand | Init;
+
+enum Hand {
+    Rock,
+    Paper,
+    Scissors
 }
 
 const initialState : State = {
-    selections: 0
+    userSelection: null,
+    aiSelection: Hand.Paper
 };
 
-const store = createStore(update, initialState);
+const update = (oldState : State, msg : Msg) : State => { 
+    const state = clone(oldState);
+
+    switch (msg.type) {
+        case 'selectHand':
+            state.userSelection = msg.hand;
+            return state;
+
+        case '@@redux/INIT':
+            return initialState;
+
+        case 'noOp':
+            return state;
+    }
+};
+
+interface State {
+    userSelection: Hand | null,
+    aiSelection: Hand
+}
+
+const store = createStore(update);
 
 interface View {
     (payload: {state : State, actions : Actions}): JSX.Element;
 }
 
-const view : View = ({state, actions}) => (<div> tic tac toe</div>);
+const view : View = ({state, actions}) => 
+(<div> 
+    <div> selected {state.userSelection} </div>
+    <button onClick={() => actions.selectHand(Hand.Rock)}> Rock </button>
+    <button onClick={() => actions.selectHand(Hand.Paper)}> Paper </button>
+    <button onClick={() => actions.selectHand(Hand.Scissors)}> Scissors </button>
+ </div>);
 
-interface Actions {}
+interface Actions {
+    selectHand(hand: Hand) : void;
+}
 
-const actions : Actions = {};
+function dispatch(msg : Msg) {
+    store.dispatch(msg);
+}
+
+const actions : Actions = {
+    selectHand(hand) {
+        dispatch({ 
+           type: 'selectHand',
+           hand
+        });
+    }
+};
 
 const App = connect((state : State) => ({state}), () => ({actions}))(view);
 
